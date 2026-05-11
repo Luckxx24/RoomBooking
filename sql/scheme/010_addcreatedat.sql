362431ae360180c681f475ab1607cae75a62c5a1
Lucky Rahman <luckyrahman911@gmail.com>
2026-05-11 21:38:32 +0700

    updating table

---
+++ sql/scheme/010_addcreatedat.sql
@@ -0,0 +1,30 @@
+-- +goose up
+alter table users add column updated_at timestamptz not null default now();
+
+alter table rooms add column created_at timestamptz not null default now();
+alter table rooms add column updated_at timestamptz not null default now();
+
+alter table fasilitas_ruangan add column created_at timestamptz not null default now();
+alter table fasilitas_ruangan add column updated_at timestamptz not null default now();
+
+alter table fasilitas add column created_at timestamptz not null default now();
+alter table fasilitas add column updated_at timestamptz not null default now();
+
+alter table booking add column created_at timestamptz not null default now();
+alter table booking add column updated_at timestamptz not null default now();
+
+-- +goose down 
+alter table users drop column updated_at timestamptz not null default now();
+
+alter table rooms drop  column created_at timestamptz not null default now();
+alter table rooms drop  column updated_at timestamptz not null default now();
+
+alter table fasilitas_ruangan drop  column created_at timestamptz not null default now();
+alter table fasilitas_ruangan drop  column updated_at timestamptz not null default now();
+
+alter table fasilitas drop column created_at timestamptz not null default now();
+alter table fasilitas drop  column updated_at timestamptz not null default now();
+
+alter table booking drop  column created_at timestamptz not null default now();
+alter table booking drop column updated_at timestamptz not null default now();
+
